355bff194a74a30de7b4643018a593dde87434e3
Fabian Martins Da Silva <fabian.martins@gmail.com>
2021-07-21 20:40:27 +0000

    Ongoing creation of the Single Region DR lab

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ npm audit fix
 npm run build
 cdk synth > output.yaml
 cdk bootstrap aws://$(eval "aws sts get-caller-identity --query "Account" --output text")/$(eval "aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]'") 
-cdk deploy --require-approval never
+cdk deploy TradingStack --require-approval never
